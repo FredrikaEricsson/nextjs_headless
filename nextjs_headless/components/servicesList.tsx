c54387fe8e5a9e565c79a63d637c8b5b7b7e0912
fredrikaericsson <fredrika@finnvid.se>
2021-10-26 13:27:59 +0200

    Added styling to services page

--- nextjs_headless/components/servicesList.tsx
+++ nextjs_headless/components/servicesList.tsx
@@ -1,4 +1,5 @@
 import ServicesItem from "./servicesItem";
+import styles from "../styles/Services.module.css";
 
 interface IServicesData {
   id: string;
@@ -15,16 +16,24 @@ interface IServicesProps {
 
 const ServicesList = ({ servicesData }: IServicesProps) => {
   return (
-    <ul>
-      {servicesData
-        .slice()
-        .reverse()
-        .map((servicesItem) => (
-          <li key={servicesItem.id}>
-            <ServicesItem servicesItem={servicesItem}></ServicesItem>
-          </li>
-        ))}
-    </ul>
+    <>
+      <h2 className={styles.pageHeadingWrapper}>Mot en ny framtid</h2>
+      <span className={styles.pageDescriptionWrapper}>
+        Tillsammans med dig gör vi ditt företag redo för framtiden. Läs mer om
+        vårt erbjudande inom fyra tjänsteområden.
+      </span>
+
+      <ul className={styles.listWrapper}>
+        {servicesData
+          .slice()
+          .reverse()
+          .map((servicesItem) => (
+            <li key={servicesItem.id}>
+              <ServicesItem servicesItem={servicesItem}></ServicesItem>
+            </li>
+          ))}
+      </ul>
+    </>
   );
 };
 
